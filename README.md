2c1435a9f9462ac9241c4372f25d205e4f1ff467
Anthony <anthonnetworks@gmail.com>
2013-05-21 10:43:40 +0200

    Init

---
+++ README.md
@@ -0,0 +1,9 @@
+Minicraft
+=========
+
+Projet du jeu Mincraft.
+
+Requis :
+--------
+* Java 7
+* jMonkeyEngine 3 (build 2013-05-21)
---
+++ src/fenetre/Minicraft.java
@@ -0,0 +1,22 @@
+package fenetre;
+
+import com.jme3.app.SimpleApplication;
+
+public class Minicraft extends SimpleApplication {
+
+	@Override
+	public void simpleInitApp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Minicraft minicraft = new Minicraft();
+		minicraft.start();
+	}
+
+}
