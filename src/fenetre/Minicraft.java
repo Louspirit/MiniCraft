e22334f91d0efa14d5ddef2995aebbb3fb5e8e47
Benjamin <Benjamin@Benjamin-PC>
2013-05-24 14:58:11 +0200

    Commit après fusion (merge)

--- src/fenetre/Minicraft.java
+++ src/fenetre/Minicraft.java
@@ -5,14 +5,22 @@ import World.MapControl;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.scene.Node;
+import com.jme3.bullet.BulletAppState;
 
 public class Minicraft extends SimpleApplication {
 	
 	private IMapControl mapControl;
-
+	// Gestion de la physique
+	private BulletAppState bulletAppState;
+	
 	@Override
 	public void simpleInitApp() {
-		
+		/** Initialise la physique (collisions) */
+	    bulletAppState = new BulletAppState();
+	    stateManager.attach(bulletAppState);
+	    /** En cas de débugage **/
+	    //bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+	    
 		mapControl = new MapControl();
 		mapControl.init(this);
 		flyCam.setMoveSpeed(80);
