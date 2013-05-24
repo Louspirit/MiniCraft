13d4625f92cfc7857270e906e834fcc18d3bb90c
Victor Roulleau <vno4@laposte.net>
2013-05-24 14:16:53 +0200

    Ajout du PlayerControl et ajout de module de gestion de la physique dans Minicraft.

--- src/fenetre/Minicraft.java
+++ src/fenetre/Minicraft.java
@@ -1,12 +1,20 @@
 package fenetre;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.bullet.BulletAppState;
 
 public class Minicraft extends SimpleApplication {
-
+	// Gestion de la physique
+	private BulletAppState bulletAppState;
+	
 	@Override
 	public void simpleInitApp() {
-		
+		/** Initialise la physique (collisions) */
+	    bulletAppState = new BulletAppState();
+	    stateManager.attach(bulletAppState);
+	    /** En cas de débugage **/
+	    //bulletAppState.getPhysicsSpace().enableDebug(assetManager);
+	    
 	}
 
 	/**
