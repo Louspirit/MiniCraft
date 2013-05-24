113014bbd6214fb4794a84da8fcdf3273a54bc47
Benjamin <Benjamin@Benjamin-PC>
2013-05-24 16:50:06 +0200

    Ajout de service dans interface pour la map

--- src/World/IMapControl.java
+++ src/World/IMapControl.java
@@ -1,6 +1,7 @@
 package World;
 
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 
 import fenetre.Minicraft;
 
@@ -15,5 +16,7 @@ public interface IMapControl {
 	
 	public void init(Minicraft minicraft);
 	public Node generateMap(int longueur, int largeur, int hauteur);
+	public boolean isBloc(int x, int y, int z);
+	public void createBloc(Spatial bloc);
 
 }
--- src/World/MapControl.java
+++ src/World/MapControl.java
@@ -5,6 +5,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.Node;
+import com.jme3.scene.Spatial;
 import com.jme3.scene.shape.Box;
 
 import fenetre.Minicraft;
@@ -12,15 +13,18 @@ import fenetre.Minicraft;
 public class MapControl implements IMapControl {
 	
 	private Minicraft minicraft;
+	private Boolean[][][] cartoMap;
+	public Node map;
 
 	
 	public void init(Minicraft minicraft) {
 		this.minicraft = minicraft;
+		cartoMap = new Boolean[100][100][100];
 	}
 
 	
 	public Node generateMap(int longueur, int largeur, int hauteur) {
-	 	Node carte = new Node();
+	 	map = new Node();
     	for (int i=1 ; i < longueur ; i++ ) {
     		for (int j = 1 ; j < largeur ; j++) {
     	        Box b = new Box(Vector3f.ZERO, 0.5f, 0.5f, 0.5f); // create cube shape at the origin
@@ -30,10 +34,21 @@ public class MapControl implements IMapControl {
     	          "Common/MatDefs/Misc/Unshaded.j3md");  // create a simple material
     	        mat.setColor("Color", ColorRGBA.Green);   // set color of material to blue
     	        geom.setMaterial(mat);                   // set the cube's material
-    	        carte.attachChild(geom);
+    	        map.attachChild(geom);
+    	        cartoMap[i][0][j] = true;
     		}
     	}
-    	return carte;
+    	return map;
+	}
+
+	public boolean isBloc(int x, int y, int z) {
+		return cartoMap[x][y][z];
+	}
+	
+	public void createBloc(Spatial bloc) {
+		Vector3f coord = bloc.getLocalTranslation();
+		cartoMap[(int)coord.getX()][(int)coord.getY()][(int)coord.getZ()] = true;
+		map.attachChild(bloc);
 	}
 
 }
