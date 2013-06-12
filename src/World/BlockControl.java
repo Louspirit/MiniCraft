116d8ace7434640ccc5792fbdf5c4d9f09b4bb1d
Anthony <anthonnetworks@gmail.com>
2013-06-12 19:58:41 +0200

    L'utilisation du mode forme ne cree plus de blocs a l'emplacement du joueur

--- src/World/BlockControl.java
+++ src/World/BlockControl.java
@@ -21,19 +21,8 @@ public class BlockControl implements IBlockControl {
 	}
 
 	@Override
-	public boolean put(Block block, Vector3f coordNewBloc) {
-		// TODO Auto-generated method stub
-
-		int x1 = Math.round(coordNewBloc.getX());
-        int y1 = Math.round(coordNewBloc.getY());
-        int z1 = Math.round(coordNewBloc.getZ());
-        
-        Camera cam = Minicraft.getInstance().getCamera();
-        int x2 = Math.round(cam.getLocation().getX());
-        int y2 = Math.round(cam.getLocation().getY());
-        int z2 = Math.round(cam.getLocation().getZ());
-        
-        if (!(x1 == x2 && (y1 == (y2-1) || y1 == y2) && z1 == z2))
+	public boolean put(Vector3f coordNewBloc) {
+        if (!playerIsHere(coordNewBloc))
         {
             mapControl.attachBloc(BlockFactory.createBlock(setting.getTypeBloc(), coordNewBloc));     
             return true;
@@ -47,7 +36,7 @@ public class BlockControl implements IBlockControl {
          Vector3f coordNewBloc = new Vector3f(
         		 coord.getX()+direction.x,coord.getY()+direction.y,coord.getZ()+direction.z);
                  
-         if (attachBloc && put(bloc, coordNewBloc))
+         if (attachBloc && put(coordNewBloc))
          {
              return null;    
          } 
@@ -59,76 +48,6 @@ public class BlockControl implements IBlockControl {
 	
 	public boolean deleteBloc(Block bloc) {
 		return mapControl.detachBlock(bloc);
-	}
-	
-	public boolean createLine(Vector3f start, Vector3f end) {
-		String direction = isLine(start,end);
-		if (direction.equals("ERROR")) {
-			// throw une erreur ?
-			return false;
-		}
-		
-		Block bloc = null;
-		int diff = 0;
-		
-		switch (direction) {
-		
-			case "X" :
-				int x1 = (int)start.x;
-				int x2 = (int)end.x;
-				diff = Math.abs(x1-x2);
-				if (x1 < x2){
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x+i,start.y,start.z));
-						mapControl.attachBloc(bloc);
-					}
-				} else {
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x-i,start.y,start.z));
-						mapControl.attachBloc(bloc);
-					}
-				}
-				break;
-				
-			case "Y" :
-				int y1 = (int)start.x;
-				int y2 = (int)end.x;
-				diff = Math.abs(y1-y2);
-				if (y1 < y2){
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x,start.y+i,start.z));
-						mapControl.attachBloc(bloc);
-					}
-				} else {
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x,start.y-i,start.z));
-						mapControl.attachBloc(bloc);
-					}
-				}
-				break;
-				
-			case "Z" :
-				int z1 = (int)start.x;
-				int z2 = (int)end.x;
-				diff = Math.abs(z1-z2);
-				if (z1 < z2){
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x,start.y,start.z+i));
-						mapControl.attachBloc(bloc);
-					}
-				} else {
-					for (int i=0;i<=diff;i++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(start.x,start.y,start.z-i));
-						mapControl.attachBloc(bloc);
-					}
-				}
-				break;
-				
-			default :
-				break;
-		}
-		
-		return true;
 	}
 	
 	/**
@@ -168,7 +87,7 @@ public class BlockControl implements IBlockControl {
 		int diffY = Math.abs(y1-y2);
 		int diffZ = Math.abs(z1-z2);
 		
-		Block bloc = null;
+		//Block bloc = null;
 		
 		if (x1>=x2) {
 			int tmp = x2;
@@ -189,55 +108,50 @@ public class BlockControl implements IBlockControl {
 		}
 		
 		if (!full) {
-
-		for (int i=0 ; i <= diffX ; i++) {
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1+i,y1,z1));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1+i,y1,z2));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2-i,y2,z1));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2-i,y2,z2));
-			mapControl.attachBloc(bloc);
-		}
+			for (int i=0 ; i <= diffX ; i++) {
+				put(new Vector3f(x1+i,y1,z1));
+				put(new Vector3f(x1+i,y1,z2));
+				put(new Vector3f(x2-i,y2,z1));
+				put(new Vector3f(x2-i,y2,z1));
+			}
 		
-
-		
-		for (int i=0 ; i <= diffY ; i++) {
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1,y1+i,z1));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1,y1+i,z2));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2,y2-i,z1));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2,y2-i,z2));
-			mapControl.attachBloc(bloc);
-		}
-	
-		
-		for (int i=0 ; i <= diffZ ; i++) {
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1,y1,z1+i));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1,y2,z1+i));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2,y1,z2-i));
-			mapControl.attachBloc(bloc);
-			bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x2,y2,z2-i));
-			mapControl.attachBloc(bloc);
-		}
-		
+			for (int i=0 ; i <= diffY ; i++) {
+				put(new Vector3f(x1,y1+i,z1));
+				put(new Vector3f(x1,y1+i,z2));
+				put(new Vector3f(x2,y2-i,z1));
+				put(new Vector3f(x2,y2-i,z2));
+			}
+			
+			for (int i=0 ; i <= diffZ ; i++) {
+				put(new Vector3f(x1,y1,z1+i));
+				put(new Vector3f(x1,y2,z1+i));
+				put(new Vector3f(x2,y1,z2-i));
+				put(new Vector3f(x2,y2,z2-i));
+			}
 		} else {
 			
 			for (int i=0 ; i <= diffX ; i++) {
 				for (int j=0 ; j <= diffY ; j++) {
 					for (int k=0 ; k <= diffZ ; k++) {
-						bloc = BlockFactory.createBlock(setting.getTypeBloc(), new Vector3f(x1+i,y1+j,z1+k));
-						mapControl.attachBloc(bloc);
+						put(new Vector3f(x1+i,y1+j,z1+k));
 					}
 				}
 			}
 		}
 		
 	}
-
+	
+	private boolean playerIsHere(Vector3f coordNewBloc) {
+		int x1 = Math.round(coordNewBloc.getX());
+        int y1 = Math.round(coordNewBloc.getY());
+        int z1 = Math.round(coordNewBloc.getZ());
+        
+        Camera cam = Minicraft.getInstance().getCamera();
+        int x2 = Math.round(cam.getLocation().getX());
+        int y2 = Math.round(cam.getLocation().getY());
+        int z2 = Math.round(cam.getLocation().getZ());
+		
+		return (x1 == x2 && (y1 == (y2-1) || y1 == y2) && z1 == z2);
+	}
+	
 }
